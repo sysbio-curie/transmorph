363882aa40b4e26ebd676b4da40b529a7b162da3
risitop <aziz.fouche@curie.fr>
2021-06-03 10:24:23 +0200

    Add link to src/

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Risitop/transmorph",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where="src"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
