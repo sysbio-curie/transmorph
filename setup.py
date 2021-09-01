23cea3132eb4fefc9b40a82ba59af7af717feccf
risitop <aziz.fouche@gmail.com>
2021-09-01 17:26:49 +0200

    Updated version number, ready for PyPi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="transmorph",
-    version="0.1.0",
+    version="0.1.1",
     author="Aziz Fouché (Institut Curie, Paris)",
     author_email="aziz.fouche@curie.fr",
     description="Optimal transport-based tools for data integration.",
