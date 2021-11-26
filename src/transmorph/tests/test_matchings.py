fe14d2c7ff927851aad5152fc3931ae0f5de38eb
loicchadoutaud <chadoutaudloic@gmail.com>
2021-11-26 15:51:35 +0100

    Add fused GW code and change the organization of the folder.

--- src/transmorph/tests/test_matchings.py
+++ src/transmorph/tests/test_matchings.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
 
-import pytest
 import numpy as np
 from scipy.sparse import csr_matrix
 
 from ..datasets import load_spirals
 from ..matching.matchingEMD import MatchingEMD
 
+
 def test_MatchingEMD():
+    """
+    Test function for test related to matching.
+    """
     xs, yt = load_spirals()
     matching = MatchingEMD()
     matching.match(xs, yt)
     assert matching.fitted
     result = matching.get(0, 1)
     assert type(result) == csr_matrix
-    matching = MatchingEMD(metric="cosine") # Test metric argument
+    matching = MatchingEMD(metric="cosine")  # Test metric argument
     matching.match(xs, yt)
     assert matching.fitted
-    matching = MatchingEMD(max_iter=int(1e4)) # Test max_iter argument
+    matching = MatchingEMD(max_iter=int(1e4))  # Test max_iter argument
     matching.match(xs, yt)
     assert matching.fitted
-    matching = MatchingEMD(use_sparse=False) # Test sparsity argument
+    matching = MatchingEMD(use_sparse=False)  # Test sparsity argument
     matching.match(xs, yt)
     assert matching.fitted
     result = matching.get(0, 1)
     assert type(result) == np.ndarray
-    
