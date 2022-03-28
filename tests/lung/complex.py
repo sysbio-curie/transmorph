e4715ce8f5415890cdc063c58cf6f1c983f889f7
risitop <aziz.fouche@gmail.com>
2022-03-28 14:22:03 +0200

    Complex full-scale pipeline test

--- tests/lung/complex.py
+++ tests/lung/complex.py
@@ -10,7 +10,7 @@ from transmorph.layers import (
     TransmorphPipeline,
 )
 from transmorph.matching import MatchingMNN
-from transmorph.merging import MergingMDI
+from transmorph.merging import MergingLinearCorrection, MergingMDI
 from transmorph.preprocessing import PPStandardize, PPPCA
 from transmorph.subsampling import SubsamplingVertexCover
 
@@ -27,15 +27,19 @@ subsampling = SubsamplingVertexCover(n_neighbors=10)
 linput = LayerInput(verbose=VERBOSE)
 lppstd = LayerPreprocessing(preprocessing=PPStandardize(True, True), verbose=VERBOSE)
 lpppca = LayerPreprocessing(preprocessing=PPPCA(n_components=30), verbose=VERBOSE)
-lmatch = LayerMatching(matching=MatchingMNN(subsampling=subsampling), verbose=VERBOSE)
-lmerge = LayerMerging(merging=MergingMDI(), verbose=VERBOSE)
+lmatch1 = LayerMatching(matching=MatchingMNN(subsampling=subsampling), verbose=VERBOSE)
+lmerge1 = LayerMerging(merging=MergingLinearCorrection(n_neighbors=10), verbose=VERBOSE)
+lmatch2 = LayerMatching(matching=MatchingMNN(subsampling=subsampling), verbose=VERBOSE)
+lmerge2 = LayerMerging(merging=MergingMDI(), verbose=VERBOSE)
 lout = LayerOutput(verbose=VERBOSE)
 
 linput.connect(lppstd)
 lppstd.connect(lpppca)
-lpppca.connect(lmatch)
-lmatch.connect(lmerge)
-lmerge.connect(lout)
+lpppca.connect(lmatch1)
+lmatch1.connect(lmerge1)
+lmerge1.connect(lmatch2)
+lmatch2.connect(lmerge2)
+lmerge2.connect(lout)
 
 pipeline = TransmorphPipeline(verbose=VERBOSE)
 pipeline.initialize(linput)
@@ -78,7 +82,7 @@ plt.xticks([])
 plt.yticks([])
 plt.xlabel("MDI1")
 plt.ylabel("MDI2")
-plt.savefig(f"{os.getcwd()}/transmorph/tests/lung/figures/simple_pertype.png")
+plt.savefig(f"{os.getcwd()}/transmorph/tests/lung/figures/complex_pertype.png")
 plt.show()
 
 plt.figure()
@@ -95,5 +99,5 @@ plt.xticks([])
 plt.yticks([])
 plt.xlabel("MDI1")
 plt.ylabel("MDI2")
-plt.savefig(f"{os.getcwd()}/transmorph/tests/lung/figures/simple_perpatient.png")
+plt.savefig(f"{os.getcwd()}/transmorph/tests/lung/figures/complex_perpatient.png")
 plt.show()
