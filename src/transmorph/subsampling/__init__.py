fd4b2fc3a4acf079f9c96dd05bec3f82849ff1ab
risitop <aziz.fouche@gmail.com>
2022-04-21 15:41:57 +0200

    deleted subsampling/__init__.py

--- src/transmorph/subsampling/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from .subsamplingKeepAll import SubsamplingKeepAll
-from .subsamplingVertexCover import SubsamplingVertexCover
-
-__all__ = ["SubsamplingKeepAll", "SubsamplingVertexCover"]
