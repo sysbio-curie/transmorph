ff6330cf1265dc3d85f53e03f273a5c8e889cb5d
risitop <aziz.fouche@gmail.com>
2022-04-22 14:10:14 +0200

    MatchingOT test

--- tests/engine/matching/test_matchingot.py
+++ tests/engine/matching/test_matchingot.py
@@ -6,21 +6,27 @@ from transmorph.stats import edge_accuracy
 from transmorph.utils import plot_result
 
 
-def test_matching_emd_accuracy():
+def test_matching_ot_accuracy():
     # Tests matching quality of EMD on small controlled dataset
     datasets = load_test_datasets_small()
     src, ref = datasets["src"], datasets["ref"]
     for optimizer in ("emd", "sinkhorn", "partial"):
-        mt = MatchingOT(optimizer=optimizer)
+        kwargs = {}
+        if optimizer == "partial":
+            kwargs["partial_transport_mass"] = 0.5
+        mt = MatchingOT(optimizer=optimizer, **kwargs)
         results = mt.fit([src.X, ref.X])
         T = results[0, 1]
+        time = mt.get_time_spent() * 1000
         accuracy = edge_accuracy(src, ref, T, "class")
         # assert accuracy >= 0.65
         plot_result(
             datasets=[src, ref],
             matching_mtx=T,
             color_by="class",
-            title=f"Optimal transport matching [acc={'{:.2f}'.format(accuracy)}]",
+            title="Optimal transport matching\n"
+            f"[acc={'{:.2f}'.format(accuracy)}, "
+            f"time={'{:.2f}'.format(time)}ms]",
             xlabel="Feature 1",
             ylabel="Feature 2",
             show=False,
@@ -31,4 +37,4 @@ def test_matching_emd_accuracy():
 
 
 if __name__ == "__main__":
-    test_matching_emd_accuracy()
+    test_matching_ot_accuracy()
