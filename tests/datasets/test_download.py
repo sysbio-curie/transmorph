fb97a37a3cf89c1e9cf221e2dbb6ad78c5c59e14
risitop <aziz.fouche@gmail.com>
2022-10-18 18:09:11 +0200

    Marking download tests as non-mandatory

--- tests/datasets/test_download.py
+++ tests/datasets/test_download.py
@@ -2,23 +2,33 @@
 
 from transmorph.datasets import load_bank, remove_bank
 
+ENABLE = False
+
 
 def test_download_chen10x():
+    if not ENABLE:
+        return
     remove_bank("chen_10x")
     _ = load_bank("chen_10x")
 
 
 def test_download_pal10x():
+    if not ENABLE:
+        return
     remove_bank("pal_10x")
     _ = load_bank("pal_10x")
 
 
 def test_download_travaglini10x():
+    if not ENABLE:
+        return
     remove_bank("travaglini_10x")
     _ = load_bank("travaglini_10x")
 
 
 def test_download_zhou10x():
+    if not ENABLE:
+        return
     remove_bank("zhou_10x")
     _ = load_bank("zhou_10x")
 
